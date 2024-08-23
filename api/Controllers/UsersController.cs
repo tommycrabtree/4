209782ff234aee4799357473de90ce743270269f
Tommy <tommycrabtree@outlook.com>
2024-08-23 14:17:48 -0400

    End of Section 8

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,27 +1,25 @@
-using api.Data;
-using api.Entities;
+using api.DTOs;
+using api.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet] // /api/users
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await context.Users.ToListAsync();
+        var users = await userRepository.GetMembersAsync();
 
-        return users;
+        return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id:int}")] // /api/users/1
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    [HttpGet("{username}")] // /api/users/1
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetMemberAsync(username);
 
         if (user == null) return NotFound();
 
---
+++ api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using api.DTOs;
+using api.Entities;
+using api.Extensions;
+using AutoMapper;
+
+namespace api.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+            .ForMember(d => d.PhotoUrl, o =>
+                o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+        CreateMap<Photo, PhotoDto>();
+    }
+}
